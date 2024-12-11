5a8f540a088667e4a3c2e3f5517e38cdd953ac32
Dmytro Sekhno <dmytro.sekhno@semantrum.net>
2024-12-11 13:33:28 +0200

    dev - pre-commit before #  ng add @ngx-pwa/local-storage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {MatNativeDateModule} from '@angular/material/core';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    importProvidersFrom(MatNativeDateModule)
+    importProvidersFrom(MatNativeDateModule), provideClientHydration(withEventReplay())
   ]
 };
